94b2bbad5deebb240687bca7e645bdfdf03cd522
Ibrahimkhanlimurad <45857481+Ibrahimkhanlimurad@users.noreply.github.com>
2021-03-15 18:36:50 +0400

    Login (#8)
    * Add login page
    
    * Add Create Event page
    
    Co-authored-by: KananHabibli <habiblikanan@gmail.com>
    Co-authored-by: ibrahimkhanlimurad <ibrahimxanlimurad@hotmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles/styles.scss";
+import './styles/styles.scss'
+import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import App from "./views/App";
 import reportWebVitals from "./reportWebVitals";
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -7,22 +7,25 @@ const Rooms = lazy(() => import("./Rooms"));
 const Grades = lazy(() => import("./Grades"));
 const TimeTable = lazy(() => import("./TimeTable"));
 // const RoomsInside = lazy(() => import("./RoomsInside"));
-
+const Login = lazy(() => import("./Login"));
+const CreateEvent = lazy(() => import("./Event/CreateEvent"))
 function App() {
   return (
     <Router>
       <div className="App">
         <React.Suspense fallback={<p>Loading...</p>}>
-          <Layout>
+          {window.location.pathname === "/login" ? ( <Route exact path="/login" component={Login} />) : (<Layout>
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route exact path="/event/create" component={CreateEvent} />
               <Route exact path="/Rooms" component={Rooms} />
               <Route exact path="/Grades" component={Grades} />
               <Route exact path="/rooms" component={Rooms} />
               {/* <Route exact path="/roomsInside" component={RoomsInside} /> */}
               <Route exact path="/time-table" component={TimeTable} />
             </Switch>
-          </Layout>
+          </Layout>)}
+          
         </React.Suspense>
       </div>
     </Router>
