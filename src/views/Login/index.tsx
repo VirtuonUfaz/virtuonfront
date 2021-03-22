29765c9a0b21d9c9f0c87cff1a6b53aea865cc46
Turqay <bmlk3644j@gmail.com>
2021-03-22 15:49:36 +0400

    Fix login errors/css

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,110 +1,53 @@
 import { useState, useContext } from "react";
-import { VirtuonContext } from "../../helpers/context/context";
+import { VirtuonContext } from "helpers/context/context";
 import { useForm } from "react-hook-form";
+import { post } from "helpers/server";
+import { fetchUser } from "helpers/actions/auth";
+import { useHistory } from "react-router-dom";
+//image
+import Background from "assets/Icons/background.svg";
+import Logo from "assets/Icons/Logo.svg";
 
 // style
 import "./style.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-//image
-import Background from "../../assets/Icons/background.svg";
-import Logo from "../../assets/Icons/Logo.svg";
-import { post } from "helpers/server";
-// import { tokenToString } from "typescript";
-import { fetchUser } from "helpers/actions/auth";
-import { useHistory } from "react-router-dom";
 
 const Login = () => {
   let history = useHistory();
   const { register, handleSubmit, errors } = useForm();
+
+  const {
+    register: register2,
+    errors: errors2,
+    handleSubmit: handleSubmit2,
+  } = useForm();
   const { setUser } = useContext(VirtuonContext);
   const [step, setStep] = useState("ID");
   const [ID, setID] = useState<string>("");
   const [OTP, setOTP] = useState<string>("");
 
-  const submit = (data): Promise<any> => {
-    if (step === "ID") {
-      return post("/auth/check", { ID: ID }, "")
-        .then((res: any) => {
-          console.log("res1: ", res);
-          if (res.data.status === 200) setStep("OTP");
-        })
-        .catch((err) => console.log("err: ", err));
-    } else if (step === "OTP") {
-      return post("/auth/login", { ID: ID, OTP: OTP }, "")
-        .then((res: any) => {
-          console.log("res: ", res);
-          if (res.data.status === 200) {
-            localStorage.setItem("token", res?.data?.token);
-            fetchUser()
-              .then((user) => setUser(user))
-              .finally(() => {
-                history.push("/home");
-              });
-          }
-        })
-        .catch((err) => console.log("err: ", err));
-    }
-    return Promise.reject();
+  const onSubmit = (data) => {
+    post("/auth/check", { ID: ID }, "")
+      .then((res: any) => {
+        if (res.data.status === 200) setStep("OTP");
+      })
+      .catch((err) => console.log("err: ", err));
   };
+  const onSubmitOTP = (data) => {
+    console.log(data);
 
-  const renderIDInput = () => (
-    <div>
-      <label className="form-label login-form-label">
-        Please Enter your ID number
-      </label>
-      <input
-        type="text"
-        name="id"
-        onChange={(val) => setID(val.target.value)}
-        placeholder="Your ID number"
-        style={errors.value && { border: "1px solid #EB5757" }}
-        className={[
-          "form-input",
-          errors.value ? "login-form-input-error" : "login-form-input",
-        ]
-          .join()
-          .replace(",", " ")}
-        ref={register({
-          required: {
-            value: true,
-            message: "An ID number is required, please enter your ID number",
-          },
-        })}
-      />
-      <span className="login-error">{errors.id?.message}</span>
-      <input type="submit" className="login-button" value="Submit" />
-    </div>
-  );
-
-  const renderOTPInput = () => (
-    <div>
-      <label className="form-label login-form-label">
-        Please enter your one-time password from your e-mail
-      </label>
-      <input
-        type="text"
-        name="otp"
-        onChange={(val) => setOTP(val.target.value)}
-        placeholder="******"
-        style={errors.value && { border: "1px solid #EB5757" }}
-        className={[
-          "form-input",
-          errors.value ? "login-form-input-error" : "login-form-input",
-        ]
-          .join()
-          .replace(",", " ")}
-        ref={register({
-          minLength: {
-            value: 6,
-            message: "An OTP is required, please enter your one-time password",
-          },
-        })}
-      />
-      <span className="login-error">{errors.id?.message}</span>
-      <input type="submit" className="login-button" value="Log in" />
-    </div>
-  );
+    post("/auth/login", { ID: ID, OTP: OTP }, "")
+      .then((res: any) => {
+        if (res.data.status === 200) {
+          localStorage.setItem("token", res?.data?.token);
+          fetchUser()
+            .then((user) => setUser(user))
+            .finally(() => {
+              history.push("/home");
+            });
+        }
+      })
+      .catch((err) => console.log("err: ", err));
+  };
 
   return (
     <main className="flex login-container">
@@ -116,8 +59,68 @@ const Login = () => {
       </div>
       <div className="login-form">
         <h1 className="login-form-title">Login to your Virtual University</h1>
-        <form onSubmit={handleSubmit((d) => submit(d))}>
-          {step === "ID" ? renderIDInput() : renderOTPInput()}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={step === "ID" ? "" : "hidden"}
+        >
+          <div>
+            <label className="form-label login-form-label">
+              Please Enter your ID number
+            </label>
+            <input
+              type="text"
+              name="id"
+              onChange={(val) => setID(val.target.value)}
+              placeholder="Your ID number"
+              style={errors.value && { border: "1px solid #EB5757" }}
+              className={[
+                "form-input",
+                errors.value ? "login-form-input-error" : "login-form-input",
+              ]
+                .join()
+                .replace(",", " ")}
+              ref={register({
+                required: {
+                  value: true,
+                  message:
+                    "An ID number is required, please enter your ID number",
+                },
+              })}
+            />
+            <span className="login-error">{errors.id?.message}</span>
+            <input type="submit" className="login-button" value="Submit" />
+          </div>
+        </form>
+        <form
+          onSubmit={handleSubmit2(onSubmitOTP)}
+          className={step === "ID" ? "hidden" : ""}
+        >
+          <div>
+            <label className="form-label login-form-label">
+              Please enter your one-time password from your e-mail
+            </label>
+            <input
+              type="text"
+              name="otp"
+              onChange={(val) => setOTP(val.target.value)}
+              placeholder="******"
+              style={errors2.value && { border: "1px solid #EB5757" }}
+              className={[
+                "form-input",
+                errors2.value ? "login-form-input-error" : "login-form-input",
+              ]
+                .join()
+                .replace(",", " ")}
+              ref={register2({
+                required: {
+                  value: true,
+                  message: "An OTP  is required, please enter your OTP number",
+                },
+              })}
+            />
+            <span className="login-error">{errors2.otp?.message}</span>
+            <input type="submit" className="login-button" value="Log in" />
+          </div>
         </form>
       </div>
     </main>
